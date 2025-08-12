087bc3ffa2e7e0fd65d7fc23fd9ff6e4860abcfd
venky <venky@gmail.com>
2025-08-12 23:09:52 +0530

    updated

---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "existing_instance_id" {
+  type    = string
+  default = "i-0112da961592de15c" # Replace this with your actual EC2 instance ID
+  description = "The ID of the already running EC2 instance used to create an AMI"
+}
+
