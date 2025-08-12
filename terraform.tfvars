617c30d2e664abe6730ed5cdd451b9d8f4d21614
venky <venky@gmail.com>
2025-08-12 23:22:52 +0530

    hhh

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+existing_instance_id = "i-03543b6f498f8ffaa"  # your real instance ID here
+
--- variables.tf
+++ variables.tf
@@ -1,6 +1,5 @@
 variable "existing_instance_id" {
-  type    = string
-  default = "i-0112da961592de15c" # Replace this with your actual EC2 instance ID
-  description = "The ID of the already running EC2 instance used to create an AMI"
+  type        = string
+  description = "ID of the existing Magento app instance for AMI creation"
 }
 
